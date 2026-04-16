f54abe0a65af7734c44565a2b0a12aa5003fdaa4
Gabriel Gado Marcolin <200774@upf.br>
2026-04-16 19:37:49 -0300

    Criada estrutura de pastas do backend

---
+++ backend/src/main/java/com/psicotestes/psicotestes/PsicotestesApplication.java
@@ -0,0 +1,13 @@
+package com.psicotestes.psicotestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicotestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicotestesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/psicotestes/psicotestes/PsicotestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psicotestes.psicotestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicotestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
