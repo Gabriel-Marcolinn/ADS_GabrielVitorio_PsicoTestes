c4b5464be6717c74ec01d7418d040fd958b321e8
Gabriel Gado Marcolin <200774@upf.br>
2026-04-16 20:28:36 -0300

    Ajustada estrutura de pastas

--- backend/src/main/java/com/psicotestes/PsicotestesApplication.java
+++ backend/src/main/java/com/psicotestes/PsicotestesApplication.java
@@ -1,4 +1,4 @@
-package com.psicotestes.psicotestes;
+package com.psicotestes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
