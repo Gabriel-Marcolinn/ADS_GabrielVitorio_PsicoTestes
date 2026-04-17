22f7be9b35c6018d071ef3cf70545d2d3ac38475
riovito <200763@upf.br>
2026-04-16 21:53:09 -0300

    instalado windi css

---
+++ frontend/windi.config.js
@@ -0,0 +1,15 @@
+// windi.config.js
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6C63FF",
+        secondary: "#F50057",
+        background: "#F5F5F5",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+};
