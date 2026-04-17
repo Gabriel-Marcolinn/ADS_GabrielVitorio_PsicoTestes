9bc6270b772be8b7e0e762b13d198880c9ef86e2
Gabriel Marcolin <gabrielgmarcolin12@gmail.com>
2026-04-16 21:02:52 -0300

    Criados os primeiros Modelos de mapeamento para o banco de dados

--- backend/src/main/java/com/psicotestes/model/Empresa.java
+++ backend/src/main/java/com/psicotestes/model/Empresa.java
@@ -1,4 +1,29 @@
 package com.psicotestes.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Table(name="empresas")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Empresa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cnpj", nullable = false, unique = true)
+    private String cnpj;
+
+    @Column(name = "razao_social", nullable = false)
+    private String razaoSocial;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "empresa")
+    private List<Usuario> usuarios;
 }
--- backend/src/main/java/com/psicotestes/model/Usuario.java
+++ backend/src/main/java/com/psicotestes/model/Usuario.java
@@ -1,4 +1,43 @@
 package com.psicotestes.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Table(name="usuarios")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @Column(name = "tipo", nullable = false, length = 2)
+    private String tipo;
+
+    @Column(name = "ativo", nullable = false)
+    private Boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(name = "empresa_id", nullable = false)
+    private Empresa empresa;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "psicologo")
+    private List<Paciente> pacientes;
 }
