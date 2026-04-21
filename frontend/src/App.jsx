01db0d0968e788d324231acc00009eba6ef7021d
rioVito <vitoriobeti@gmail.com>
2026-04-21 11:17:35 -0300

    aplicada estilizacao na pagina principal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,59 @@
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 
-function App() {
+export default function App() {
   const navigate = useNavigate();
   return (
-    <>
-      <h1>Você está logado! Bem vindo ao app.</h1>
-      <Button variant="contained" onClick={() => navigate("/empresas")}>
-        Empresas
-      </Button>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <Typography
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            mb: 2,
+          }}
+          variant="h5"
+          fontWeight="bold"
+        >
+          Você está logado! Bem vindo ao app.
+        </Typography>
+        <Paper elevation={4} sx={{ p: 2, width: 600, borderRadius: 3 }}>
+          <Typography
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              mb: 2,
+            }}
+            variant="h5"
+            fontWeight="bold"
+          >
+            Opcoes:
+          </Typography>
+          <Button variant="contained" onClick={() => navigate("/empresas")}>
+            Empresas
+          </Button>
+        </Paper>
+      </Box>
+    </Box>
   );
 }
-
-export default App;
