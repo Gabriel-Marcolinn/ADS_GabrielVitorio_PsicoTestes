9be50248adb6b31fd0123bec0af0a984731a5bfd
MariaVitoriaK <197960@upf.br>
2026-06-23 20:23:26 -0300

    Ajustado SQL padrão de inserção de clinica e usuário inicial, para que o mesmo indique corretamente dados que atendam às novas regras de negócio do sistema

--- backend/src/main/resources/sql/users.sql
+++ backend/src/main/resources/sql/users.sql
@@ -4,12 +4,12 @@
 
 -- 1. Inserir a Empresa Matriz (Necessário para a chave estrangeira empresa_id no Usuário)
 INSERT INTO empresas (id, razao_social, cnpj, ativo, deletado)
-VALUES (1, 'Clínica Matriz PsicoTestes', '00000000000000', true, false);
+VALUES (1, 'Clínica Matriz PsicoTestes', '12345678000195', true, false);
 
 -- 2. Inserir o Usuário Administrador (Tipo 'AD')
--- A string na senha é o hash BCrypt exato para: "123456"
+-- A string na senha é o hash BCrypt exato para: "12345678"
 INSERT INTO usuarios (id, nome, email, senha, tipo, ativo, deletado, empresa_id)
-VALUES (1, 'Administrador Geral', 'admin@psicotestes.com.br', '$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.', 'AD', true, false, 1);
+VALUES (1, 'Administrador Geral', 'admin@psicotestes.com.br', '$2y$10$i4JkmeegcjFnBYtLIovkDONNuQj2LUnuyiQz4hg6qN3etlBBsx3s6', 'AD', true, false, 1);
 
 -- 3. Atualizar as sequences para evitar erros de "Unique Constraint" (Atualizado para empresas_id_seq)
 SELECT setval('empresas_id_seq', (SELECT MAX(id) FROM empresas));
